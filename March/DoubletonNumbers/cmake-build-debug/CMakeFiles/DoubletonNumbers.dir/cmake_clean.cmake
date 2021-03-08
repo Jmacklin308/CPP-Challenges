bb73e91dc9c406cb554b28141af9812ad0703bae
Justin Macklin <Jmacklin308>
2021-03-08 15:44:59 -0600

    Started Doubleton number Project

---
+++ March/DoubletonNumbers/cmake-build-debug/CMakeFiles/DoubletonNumbers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DoubletonNumbers.dir/main.cpp.obj"
+  "DoubletonNumbers.exe"
+  "DoubletonNumbers.exe.manifest"
+  "DoubletonNumbers.lib"
+  "DoubletonNumbers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DoubletonNumbers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
