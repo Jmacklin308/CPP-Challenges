e8748b99998f5d351282ed675686a38c18259c5d
Justin Macklin <Jmacklin308>
2021-03-08 16:00:25 -0600

    started

--- March/VolumeOfPizza/main.cpp
+++ March/VolumeOfPizza/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+/*
+ * Goal: You are given a radius and hight of a pizza. Return the volume of pizza
+ *
+ * example:
+ *  volPizza(1, 1) ➞ 3
+ *  //(radius² x height x π) = 3.14159... rounded to the nearest integer.
+ *  volPizza(7, 2) ➞ 308
+ *  volPizza(10, 2.5) ➞ 785
+ *
+ *
+ */
 
+
+
+
+//TODO: pass input to volPizza
+//TODO: create function (volPizza)
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //TODO: take input for radius and height of pizza
+    int radius{0};
+    int height{0};
+
     return 0;
 }
