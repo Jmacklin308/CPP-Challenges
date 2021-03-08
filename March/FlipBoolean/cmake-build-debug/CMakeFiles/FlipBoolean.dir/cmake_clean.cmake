3ba61c5fe0745eff801efdb0d5cff56f7af71404
Justin Macklin <Jmacklin308>
2021-03-08 14:26:24 -0600

    Started flip boolean project

---
+++ March/FlipBoolean/cmake-build-debug/CMakeFiles/FlipBoolean.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FlipBoolean.dir/main.cpp.obj"
+  "FlipBoolean.exe"
+  "FlipBoolean.exe.manifest"
+  "FlipBoolean.lib"
+  "FlipBoolean.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FlipBoolean.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ March/FlipBoolean/cmake-build-debug/CMakeFiles/FlipBoolean.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for FlipBoolean.
+# This may be replaced when dependencies are built.
