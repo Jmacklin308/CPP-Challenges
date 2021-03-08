efec2470148d5b573091086bd3b8b40021472ace
Justin Macklin <Jmacklin308>
2021-03-08 15:52:15 -0600

    Set foundation. Should finish this later when my skills are better.

--- March/DoubletonNumbers/main.cpp
+++ March/DoubletonNumbers/main.cpp
@@ -1,6 +1,29 @@
+/*
+ * //NEEDS FINISHED
+ * Goal: Create a function that takes in a number and finds the next doubleton number
+ *
+ * what is a doubleton number? A doubleton is a number that contains two distinct digits
+ * example: 23,35,100,12121
+ *
+ *
+ */
+
 #include <iostream>
 
+unsigned doubleton(unsigned int n){
+//logic here
+}
+
+
+
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int number{0};
+    //prompt for doubleton number
+    std::cout << "Hello there, please place your doubleton number (two distinct digits): \n";
+    std::cin >> number;
+    //call function
+    doubleton(number);
     return 0;
 }
