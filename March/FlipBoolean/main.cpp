2f479523d6ad8b20a3a1d42dcfc48e0d670c4121
Justin Macklin <Jmacklin308>
2021-03-08 14:46:54 -0600

    Program finished. Returns 0 if input was 1 and vice versa.

--- March/FlipBoolean/main.cpp
+++ March/FlipBoolean/main.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
 /*
  * Goal: create a function that takes a boolean value and flips it
+ *
+ *
+ *
+ *
+ *
  */
 
-
-
+//Create function called "reverse" that returns bool
+bool reverse(bool value){
+    bool flippedVal{false};
+    if(value)
+    {
+        flippedVal = false;
+    }else
+    {
+        flippedVal = true;
+    }
+    return flippedVal;
+}
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int result{0};
+    int input{0};
+
+    std::cin >> input;
+
+    result = reverse(input);
+
+    std::cout << result;
 }
