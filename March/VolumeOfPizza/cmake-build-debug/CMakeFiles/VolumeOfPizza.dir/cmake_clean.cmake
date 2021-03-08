fe3b02cac1a0ad0e58154c830ba639dd0a2a6280
Justin Macklin <Jmacklin308>
2021-03-08 15:54:48 -0600

    Started project: Calculate the volume of pizza

---
+++ March/VolumeOfPizza/cmake-build-debug/CMakeFiles/VolumeOfPizza.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VolumeOfPizza.dir/main.cpp.obj"
+  "VolumeOfPizza.exe"
+  "VolumeOfPizza.exe.manifest"
+  "VolumeOfPizza.lib"
+  "VolumeOfPizza.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VolumeOfPizza.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
