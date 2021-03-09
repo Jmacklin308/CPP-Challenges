c88452b5d8c6570c8e5733a49fd9ca703b0ec0e8
Justin Macklin <Jmacklin308>
2021-03-08 19:27:25 -0600

    Almost there. Just need to round up.

--- March/VolumeOfPizza/main.cpp
+++ March/VolumeOfPizza/main.cpp
@@ -12,18 +12,33 @@
  */
 
 #include <iostream>
-#include "math.h"
+//I like pie
 
 //TODO: pass input to volPizza
-int volPizza(int radius, float height){
+int volPizza(float radius, float height){
+    int volume{0};
+    double pi = 2 * acos(0.0);
 
+    int answer =
+    
+    volume =  (radius^2) * height * pi;
+    
+    std::cout << "The total volume of pizza is: " << pi;
 
+    return volume;
 }
 
 int main() {
     //TODO: take input for radius and height of pizza
     int radius{0};
     int height{0};
-
-    return 0;
+    
+    std::cout << "What was the radius of the pizza in question?";
+    std::cin >> radius;
+    
+    std::cout << "What was the height of the pizza in question?";
+    std::cin >> height;
+    
+    //send it
+    volPizza(radius, height);
 }
